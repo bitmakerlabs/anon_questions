2c6145cb98c88b0a24f6d82bd434d00709c9a52c
David Mills <dave@1morepx.com>
2013-03-08 15:36:13 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnonQuestions::Application.config.secret_token = 'ae06a5c12b38097147877129a0f41ed5241b50652a291970fac275bfb7142aa7237d083713d95ce73fcd5fd23e05c63820626538cabe10c6f9ff5c7537e2b06c'
